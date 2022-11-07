06e2e4802a6162677d075c43c4fd389a5ee0ca78
hayoun <fishingest@kookmin.ac.kr>
2022-11-08 01:51:27 +0900

    map,forEach generic 분석

---
+++ Third/third.ts
@@ -0,0 +1,47 @@
+
+
+
+
+// [1,2,3].forEach(item => {
+//     console.log(item);
+    
+// });
+// // 1,2,3 item은 number로 잘 추론
+
+// ['1','2','3'].forEach(item => {
+//     console.log(item);
+    
+// });
+// // string으로 추론한다
+
+// ['123',123,true].forEach(item => {
+//     console.log(item);
+    
+// });
+// // 여기선 number or boolean
+
+
+// aaa.forEach((value)=>{console.log(value)});
+
+
+// function add1<T>(x:T,y:T){
+//     return x;
+// }
+
+// add1(1,'2') // 안됨, generic으로 타입이 같을때만 실행되게 했기 때문!
+// add1('1','2') // 됨
+
+
+// add1<number>(1,2); // type parameter를 이용해서도 가능하다
+
+
+interface Array1<T> {
+    forEach(callbakfn: (value:T,index:number,array:T[])=>void,thisArg?: any):void;
+    map<U>(callbackfn:(value:T,index:number,array:T[])=>U,thisArg?:any):U[];
+}
+//선언할때, 뒤에 generic을 붙여준다!
+const aaa:Array1<number>=[1,2,3];
+
+const strings=[1,2,3].map((item=>item.toString));
+
+//item 을 number로 잘받게된다!
